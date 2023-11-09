f4c3c75fb2da452bcc1a46eeffcf2bc22b57740d
Vagner Souza <66963813+vagnersouza23@users.noreply.github.com>
2023-11-09 07:39:05 -0300

    Aula-04-finalizada

--- js/criarVideo.js
+++ js/criarVideo.js
@@ -1,4 +1,4 @@
-import { conectaApi } from "./conectaApi.js"
+import { conectaApi } from "./conectaApi.js";
 const formulario = document.querySelector("[data-formulario]")
 
 async function criarVideo(evento){
